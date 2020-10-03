8b83c6f7e642ad3a9c32eb2f40f977f4cfb2431a
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-03 16:20:14 +0530

    [Akash] Added UC2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("Welcome to Snake and Ladder");
             const int START_POSITION = 0;
+            Random random = new Random();
+            int dieRoll = random.Next(1, 7);
+            Console.WriteLine("The number appeared is {0}", dieRoll);
             Console.ReadKey();
         }
     }
