ec44f068d6dac164e4a17baf568e5d7a85f7869c
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-03 16:41:13 +0530

    [Akash] Added UC4

--- Program.cs
+++ Program.cs
@@ -11,28 +11,40 @@ namespace SnakeAndLadder
             const int LADDER = 2;
             const int SNAKE = 3;
             int currentPosition = START_POSITION;
+            int winningPlace = 100;
+            int numOfMoves = 0;
 
             Console.WriteLine("Welcome to Snake and Ladder");
-            
             Random random = new Random();
-            int dieRoll = random.Next(1, 7);
-            Console.WriteLine("The number appeared is {0}", dieRoll);
-           
-            int option = random.Next(1, 4);
-            switch(option)
-            {
-                case NO_PLAY:
-                    break;
 
-                case LADDER:
-                    currentPosition += dieRoll;
-                    break;
-                case SNAKE:
-                    currentPosition -= dieRoll;
-                    break;
 
+            while (currentPosition < winningPlace)
+            {               
+                int dieRoll = random.Next(1, 7);
+                Console.WriteLine("The number appeared is {0}", dieRoll);
+                int option = random.Next(1, 4);
+                Console.WriteLine("Next move : {0}", option);
+
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+
+                    case LADDER:
+                        currentPosition += dieRoll;
+                        break;
+                    case SNAKE:
+                        currentPosition -= dieRoll;
+                        if (currentPosition < START_POSITION)
+                        {
+                            currentPosition = START_POSITION;
+                        }
+                        break;
+                }
+                numOfMoves++;
             }
             Console.WriteLine("The current Position is {0}", currentPosition);
+            Console.WriteLine("Total moves used {0}", numOfMoves);
             Console.ReadKey();
         }
     }
