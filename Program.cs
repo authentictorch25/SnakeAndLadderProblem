4842c1c7b9f464c400ff3c4699e8681d438cb28f
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-03 16:16:13 +0530

    [Akash] Added UC1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder");
+            const int START_POSITION = 0;
             Console.ReadKey();
         }
     }
